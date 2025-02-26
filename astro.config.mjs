b10f285546608496d861ea0aba36e678fc2dbca8
IEatBeans <ieatbeans222@gmail.com>
2025-02-25 19:51:51 -0800

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,23 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
+
+	output: "server",
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true
+		}
+	}),
+
 	integrations: [
 		starlight({
 			title: 'My Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/KyGuy2002/gls-docs',
 			},
 			sidebar: [
 				{
@@ -25,4 +35,14 @@ export default defineConfig({
 			],
 		}),
 	],
+
+	vite: {
+		resolve: {
+		  // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+		  // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+		  alias: import.meta.env.PROD && {
+			"react-dom/server": "react-dom/server.edge",
+		  },
+		},
+	  },
 });
